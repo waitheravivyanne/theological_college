68411a1360fe258ccaf054547ebc25576a498982
vivian waithera <vivyannekori@gmail.com>
2025-02-26 15:57:56 +0300

    app-js

--- soul-winners-theological-college/src/App.js
+++ soul-winners-theological-college/src/App.js
@@ -1,32 +1,32 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Hero from './components/Hero';
-// import MissionVision from './components/MissionVision';
-// import Programs from './components/Programs';
-// import About from './components/About';
-// import Contact from './components/Contact';
-// import Footer from './components/Footer';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import MissionVision from './components/MissionVision';
+import Programs from './components/Programs';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
-// const App = () => {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={
-//           <>
-//             <Hero />
-//             <MissionVision />
-//             <Programs />
-//           </>
-//         } />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/programs" element={<Programs />} />
-//         <Route path="/contact" element={<Contact />} />
-//       </Routes>
-//       <Footer />
-//     </Router>
-//   );
-// };
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Hero />
+            <MissionVision />
+            <Programs />
+          </>
+        } />
+        <Route path="/about" element={<About />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
 
-// export default App;
+export default App;
